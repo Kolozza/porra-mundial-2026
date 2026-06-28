eea1d238e598e7f9d05aeb718d4cf8f73ae56281
Nikos <tu@email.com>
2026-06-28 17:03:18 +0200

    og: contain image to show full group

--- api/og.js
+++ api/og.js
@@ -5,12 +5,25 @@ export const config = { runtime: "edge" };
 
 export default function handler() {
   return new ImageResponse(
-    React.createElement("img", {
-      src: "https://porra-mundial-2026-seven-tau.vercel.app/og-image.jpg",
-      width: 1200,
-      height: 630,
-      style: { objectFit: "cover", objectPosition: "center 35%" },
-    }),
+    React.createElement(
+      "div",
+      {
+        style: {
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#0b1e0d",
+        },
+      },
+      React.createElement("img", {
+        src: "https://porra-mundial-2026-seven-tau.vercel.app/og-image.jpg",
+        width: 1200,
+        height: 630,
+        style: { objectFit: "contain" },
+      })
+    ),
     { width: 1200, height: 630 }
   );
 }
